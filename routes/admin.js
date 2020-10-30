0b08dc1b0dc642483aae7b19ddcb56c1011ffc40
iemprashant <prashantgupta9876@gmail.com>
2020-10-30 13:05:36 +0530

    working on product edit function

--- routes/admin.js
+++ routes/admin.js
@@ -1,17 +1,22 @@
-const path = require("path");
+const path = require('path')
 
-const express = require("express");
+const express = require('express')
 
-const adminController = require("../controllers/admin");
+const adminController = require('../controllers/admin')
 
-const router = express.Router();
+const router = express.Router()
 
 // /admin/add-product => GET
-router.get("/add-product", adminController.getAddProduct);
+router.get('/add-product', adminController.getAddProduct)
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct)
 
 // /admin/products => GET
-router.get("/products");
-// /admin/add-product => POST
-router.post("/add-product", adminController.postAddProduct);
+router.get('/products', adminController.getProducts)
 
-module.exports = router;
+// /admin/edit-product/productid => GET
+router.get('/edit-product/:productId', adminController.getEditProduct)
+// /admin/edit-product =>POST
+router.post('/edit-product', adminController.postEditProduct)
+module.exports = router
