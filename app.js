dd209d08dc6da7089d39fb14ac15394de3e77085
iemprashant <prashantgupta9876@gmail.com>
2020-11-22 13:22:01 +0530

    association with sequelize

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use((req, res, next) => {
-  User.findById(1)
+  User.findByPk(1)
     .then((user) => {
       req.user = user
       next()
@@ -40,7 +40,7 @@ sequelize
   // .sync({ force: true })
   .sync()
   .then((result) => {
-    return User.findById(1)
+    return User.findByPk(1)
     // console.log(result);
   })
   .then((user) => {
